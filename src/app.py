f4ab617b465b66891cd407984656f626a7b373b7
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2024-08-17 19:21:35 +0530

    Fixed Bugs in api's

--- src/app.py
+++ src/app.py
@@ -38,12 +38,12 @@ def add_item_to_menu():
 def add_item_to_cart():
     if request.is_json and  'user_name' in request.get_json() and  'number_of_items' in request.get_json() and  'item_name' in request.get_json() and  'item_cost' in request.get_json():
         data = request.get_json()
-        item_type = data['item_type']
+        user_name = data['user_name']
         number_of_items = data['number_of_items']
         item_name = data['item_name']
         item_cost = data['item_cost']
         try:
-            menu.add_item_to_cart(item_type, number_of_items, item_name, item_cost)
+            menu.add_item_to_cart(user_name, number_of_items, item_name, item_cost)
             return jsonify({"Status": 'Success'}), 200
         except Exception as e:
             return jsonify({"Status": "Failure"}), 400
@@ -74,7 +74,7 @@ def get_all_item_types():
     except Exception as e:
         return jsonify({"Status": "incorrect payload"}), 400
     
-@app.route('/all-item-in-menu/{item_type}', methods=['GET'])
+@app.route('/all-item-in-menu/<item_type>', methods=['GET'])
 def get_all_items_by_item_type(item_type):
     try :
         data = menu.get_all_items_by_item_type(item_type)
@@ -82,7 +82,7 @@ def get_all_items_by_item_type(item_type):
     except Exception as e:
         return jsonify({"Status": "incorrect payload"}), 400
     
-@app.route('/all-item-in-menu/user/{user_name}', methods=['GET'])
+@app.route('/all-item-in-menu/user/<user_name>', methods=['GET'])
 def get_items_in_cart_by_user_name(user_name):
     try :
         data = menu.get_items_in_cart_by_user_name(user_name)
@@ -118,17 +118,13 @@ def decrease_item_count():
     else:
         return jsonify({"Status": "incorrect payload"}), 400
 
-@app.route('/item-to-cart/{user_name}/{item_name}', methods=['DELETE'])
+@app.route('/item-to-cart/<user_name>/<item_name>', methods=['DELETE'])
 def remove_item_from_cart(user_name, item_name):
     try :
-        data = menu.remove_item_from_cart(user_name, item_name)
-        return jsonify(data), 200
+        menu.remove_item_from_cart(user_name, item_name)
+        return jsonify({"Status": 'Success'}), 200
     except Exception as e:
         return jsonify({"Status": "Failure"}), 400
     
 if __name__ == '__main__':
     app.run(host = config.app_host, port = config.app_port)
-
-    
-    
-           
--- src/food_menu.py
+++ src/food_menu.py
@@ -55,16 +55,16 @@ class FoodMenu:
         return {'cart_items': get_items_in_cart_by_user_name}
     
     def increase_item_count(self, user_name, item_name):
-        update_query = f"update {self.cart_table} set item_count = item_count+1 where user_name='{user_name}' and item_name = '{item_name}'"
+        update_query = f"update {self.cart_table} set number_of_items = number_of_items+1 where user_name='{user_name}' and item_name = '{item_name}'"
         self.execute_query(update_query)
     
     def decrease_item_count(self, user_name, item_name):
-        get_query = f"select item_count from {self.cart_table} where user_name = '{user_name}' and item_name = '{item_name}'"
-        item_count = self.execute_get_query(get_query)
-        if item_count>2:
-            update_query = f"update {self.cart_table} set item_count = item_count-1 where user_name='{user_name}' and item_name = '{item_name}'"
+        get_query = f"select number_of_items from {self.cart_table} where user_name = '{user_name}' and item_name = '{item_name}'"
+        item_count = self.execute_get_query(get_query).fetchone()[0]
+        if item_count>1:
+            update_query = f"update {self.cart_table} set number_of_items = number_of_items-1 where user_name='{user_name}' and item_name = '{item_name}'"
             self.execute_query(update_query)
 
     def remove_item_from_cart(self, user_name, item_name):
-        delete_query = f"delete * from {self.cart_table} where user_name = '{user_name}' and item_name = '{item_name}'"
+        delete_query = f"delete from {self.cart_table} where user_name = '{user_name}' and item_name = '{item_name}'"
         self.execute_query(delete_query)
